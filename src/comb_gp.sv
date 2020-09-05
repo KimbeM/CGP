0bf61354f5a765fca0124a1121808f71870cbd43
Kim Berg <kim.berg@danfoss.com>
2020-09-05 11:53:09 +0300

    Comments and minor changes

--- src/comb_gp.sv
+++ src/comb_gp.sv
@@ -33,6 +33,7 @@ module comb_gp;
   dot_product          dp;
   
   /*
+
   task test(input comb_circuit individual, output int out); 
     int num_tests;
     
@@ -65,7 +66,7 @@ module comb_gp;
   endtask: test
 
   
-  //PWM
+  //PWM with enable input
   task test(input comb_circuit individual, output int out);
     const int pwm_period  = 20;
     const int num_periods = 2;
@@ -102,9 +103,39 @@ module comb_gp;
     out = L1_norm;
   endtask: test
 
-
+  */
   
-  //Square wave
+  //Square wave with selectable duty cycle
+  task test(input comb_circuit individual, output int out);
+    const int peak       = 10;
+    const int num_periods = 3;
+        
+    L1_norm = 0;      
+        
+    X[0] = 0;
+        
+    for(int i=0; i<num_periods; i++)begin
+      for(int j=0; j<peak; j++)begin
+          X[1]     = Y[0];    
+          Y        = individual.evaluate_outputs(X);         
+          if(i==0)
+            Y_EXP[0] = j;
+          else if(i==1)
+            Y_EXP[0] = j*2;
+          else
+            Y_EXP[0] = 0;
+          L1_norm = L1_norm + abs(Y[0] - Y_EXP[0]);
+          if(L1_norm < 0)
+            L1_norm = 2**31-1;  //Saturate to max if L1 norm overflowed        
+          #1;
+      end
+    end
+    
+    out = L1_norm;
+  endtask: test   
+  
+  /*
+  //Square wave with selectable duty cycle
   task test(input comb_circuit individual, output int out);
     const int pwm_period  = 5;
     const int num_periods = 2;
@@ -122,7 +153,7 @@ module comb_gp;
         if(t < duty_cycle)begin
           Y_EXP[0] = 5;
         end else begin
-          Y_EXP[0] = 0;
+          Y_EXP[0] = -2;
         end
         L1_norm = L1_norm + abs(Y[0] - Y_EXP[0]);
         if(L1_norm < 0)
@@ -133,8 +164,7 @@ module comb_gp;
     
     out = L1_norm;
   endtask: test  
-
-
+  
   //Triangle wave
   task test(input comb_circuit individual, output int out);
     const int peak_val  = 8;
@@ -168,7 +198,7 @@ module comb_gp;
     out = L1_norm;
   endtask: test   
  
-  */
+  /*
   
   task stimulate_x(input bit[NUM_INPUTS-1:0] mask, input int idx);
 
@@ -198,7 +228,11 @@ module comb_gp;
      
     int result;
         
-    L1_norm = 0;      
+    L1_norm = 0;     
+
+    //X[0] increases DUT output by one
+    //X[1] resets output
+    //X[2] is a registered feedback from Y[0]
        
     //Toggle X[0] and check that Y is incremented by one after one clock cycle
     stimulate_x('b001, idx);
@@ -221,19 +255,34 @@ module comb_gp;
       check_result(individual, idx, 2, result); //Expect two
     end
     
+    //Toggle X[1] (reset) and check that Y is set to zero after one clock cycle
+    stimulate_x('b010, idx); 
+    check_result(individual, idx, 2, result); //Expect two     
+    stimulate_x('b000, idx); 
+    check_result(individual, idx, 0, result); //Expect zero (reset)     
+    
+    
     //Toggle X[0] for two cycles and check that Y is incremented by two
     stimulate_x('b001, idx);
-    check_result(individual, idx, 2, result); //Expect two     
+    check_result(individual, idx, 0, result); //Expect zero     
     stimulate_x('b001, idx);
+    check_result(individual, idx, 1, result); //Expect increment 
+    stimulate_x('b000, idx);
+    check_result(individual, idx, 2, result); //Expect increment 
+    
+    //Toggle X[0] for one cycle, check that Y is incremented by one, then reset    
+    stimulate_x('b001, idx);
+    check_result(individual, idx, 2, result); //Expect two     
+    stimulate_x('b011, idx);
     check_result(individual, idx, 3, result); //Expect increment 
     stimulate_x('b000, idx);
-    check_result(individual, idx, 4, result); //Expect increment 
-        
+    check_result(individual, idx, 0, result); //Expect zero (reset). Reset takes presedence over increment.   
 
  
     out = L1_norm;
   endtask: test     
 
+  */
   
 initial begin
 
@@ -270,8 +319,8 @@ initial begin
       population[i].clear_counters();
 
       //Test this individual
-      //test(population[i], population[i].fitness);
-      test(population[i], i, population[i].fitness);
+      test(population[i], population[i].fitness);
+      //test(population[i], i, population[i].fitness);
 
       //Replace current best solution with improved solution
       if(best_solution.fitness > population[i].fitness)begin 
@@ -283,7 +332,7 @@ initial begin
       if(population[i].fitness == 0)begin
         population[i].calc_resource_util();
         population[i].calc_score();
-        if(population[i].score < best_solution.score)begin
+        if(population[i].score < best_solution.score)begin// && population[i].score < 80)begin
           best_solution = population[i].copy();
           population[i].print_resource_util();
           //$display("Solution found in generation %2d, genotype %2d with score of %2d", gen, i, best_solution.score); 
@@ -302,8 +351,8 @@ initial begin
         offspring[k].mutate();
       
         //Test this offspring individual
-        //test(offspring[k], offspring[k].fitness);
-        test(offspring[k], i, offspring[k].fitness);
+        test(offspring[k], offspring[k].fitness);
+        //test(offspring[k], i, offspring[k].fitness);
 
         if(k == 0)
           best_offspring = offspring[k].copy;
